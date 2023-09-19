450939e695278fcaed3d1ca34c2cb868f9011cee
若海 <wang@rehiy.com>
2023-09-19 11:28:21 +0800

    perf: 简化环境变量注册

--- Dockerfile
+++ Dockerfile
@@ -4,15 +4,11 @@ LABEL version="1.1.0" \
       maintainer="wang@rehiy.com"
 
 ADD app /app
-WORKDIR /app/
 
+WORKDIR /app/
 RUN sh /app/deploy
 
 ENV PYTHONPATH=/app
-ENV TORCH_CACHE=/cache/torch
-ENV TRANSFORMERS_CACHE=/cache/transformers
-ENV EASYNMT_CACHE=/cache/easynmt
-ENV EASYNMT_MODEL=opus-mt
 
 EXPOSE 80
 
--- app/service.sh
+++ app/service.sh
@@ -4,10 +4,16 @@
 # turn on bash's job control
 set -m
 
+# Define env vars
+export TORCH_CACHE=${TORCH_CACHE:-"/cache/torch"}
+export TRANSFORMERS_CACHE=${TRANSFORMERS_CACHE:-"/cache/transformers"}
+export EASYNMT_CACHE=${EASYNMT_CACHE:-"/cache/easynmt"}
+export EASYNMT_MODEL=${EASYNMT_MODEL:-"opus-mt"}
+
 # Create cache dir
-mkdir -p $TORCH_CACHE
-mkdir -p $TRANSFORMERS_CACHE
-mkdir -p $EASYNMT_CACHE
+[ -d $TORCH_CACHE ] || mkdir -p $TORCH_CACHE
+[ -d $TRANSFORMERS_CACHE ] || mkdir -p $TRANSFORMERS_CACHE
+[ -d $EASYNMT_CACHE ] || mkdir -p $EASYNMT_CACHE
 
 # Run preset script
 echo "Run preset script ..."
