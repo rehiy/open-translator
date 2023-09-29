5cfe5877122990aea362e46007a601c4ad7fdb0b
若海 <wang@rehiy.com>
2023-09-29 14:20:57 +0800

    perf: 优化环境变量

--- server/env.js
+++ server/env.js
@@ -1,24 +1,15 @@
 import { platform } from 'os'
 
 
-// 重写环境变量
-
 const env = process.env || {};
+export default env;
 
 env.NODE_ENV = env.NODE_ENV === 'production' ? 'production' : 'developement';
 
 env.BROWSER_CHANNEL = platform() === 'win32' ? 'msedge' : '';
-env.BROWSER_HANDLESS = env.NODE_ENV == 'production';
+env.BROWSER_HANDLESS = env.NODE_ENV == 'production' ? 'true' : 'false';
 
 env.WORKER_NUMBER = env.WORKER_NUMBER > 0 ? env.WORKER_NUMBER : 1;
 
 env.LISTEN_HOST = env.LISTEN_HOST || '127.0.0.1';
 env.LISTEN_PORT = env.LISTEN_PORT || '8000';
-
-// 导出环境变量
-
-export default {
-    ...env,
-    // 修复变量类型
-    BROWSER_HANDLESS: env.BROWSER_HANDLESS === 'true'
-}
