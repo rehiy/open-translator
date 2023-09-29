bad3d2662a1a18793f36ebd28c8e5dc9032bdf87
若海 <wang@rehiy.com>
2023-09-29 16:33:36 +0800

    fix: 修复环境变量

--- app/service.sh
+++ app/service.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #
 
-[ -z "$NODE_ENV" ] && NODE_ENV=production
+[ -z "$NODE_ENV" ] && export NODE_ENV=production
 
-[ -z "$WORKER_NUMBER" ] && WORKER_NUMBER=2
+[ -z "$WORKER_NUMBER" ] && export WORKER_NUMBER=2
 
-[ -z "$LISTEN_HOST" ] && LISTEN_HOST=0.0.0.0
-[ -z "$LISTEN_PORT" ] && LISTEN_PORT=80
+[ -z "$LISTEN_HOST" ] && export LISTEN_HOST=0.0.0.0
+[ -z "$LISTEN_PORT" ] && export LISTEN_PORT=80
 
 exec npm start
